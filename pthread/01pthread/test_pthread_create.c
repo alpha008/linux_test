7ee123d772daa75683b5480a5f9b556158d6b021
alpha <zjxlove2017@163.com>
2019-06-22 22:50:00 +0800

    提交原因： 进程学习 影响模块：
    日期：2019.06.22

---
+++ pthread/01pthread/test_pthread_create.c
@@ -0,0 +1,52 @@
+﻿/*
+#include <pthread.h>
+int pthread_create( pthread_t *thread,const pthread_attr_t *attr,void *(*start_routine)(void *),void *arg );
+功能：
+创建一个线程。
+参数：
+thread：线程标识符地址。 
+attr：线程属性结构体地址，通常设置为 NULL。
+start_routine：线程函数的入口地址。
+arg：传给线程函数的参数。
+返回值：
+成功：0
+失败：非 0
+pthread_create() 创建的线程从指定的回调函数开始运行，该函数运行完后，该线程也就退出了。
+线程依赖进程存在的，共享进程的资源，如果创建线程的进程结束了，线程也就结束了。
+*/
+#include <stdio.h>  
+#include <unistd.h>  
+#include <pthread.h>  
+int var  = 8;  
+void *thread_1(void *arg)  
+{  
+    while(1)  
+    {  
+        printf("this is my new thread1: var++\n");  
+        var++;  
+        sleep(1);  
+    }  
+    return NULL;  
+}
+void *thread_2(void * arg)  
+{  
+    while(1)
+    {  
+        printf("this is my new thread2: var = %d\n", var);  
+        sleep(1);  
+    } 
+    return NULL;  
+}   
+int main(int argc, char *argv[])  
+{  
+    pthread_t tid1,tid2;    
+    //创建两个线程  
+    pthread_create(&tid1, NULL, thread_1, NULL);    
+    pthread_create(&tid2, NULL, thread_2, NULL);  
+    while(1)
+    {  
+        printf("the main thread: var = %d\n", var);  
+        sleep(1);  
+    }    
+    return 0;  
+}  
---
+++ pthread/01pthread/test_pthread_exit.c
@@ -0,0 +1,48 @@
+﻿/*
+所需头文件：
+#include <pthread.h>
+void pthread_exit(void *retval);
+功能：
+退出调用线程。一个进程中的多个线程是共享该进程的数据段，因此，通常线程退出后所占用的资源并不会释放。
+参数：retval：存储线程退出状态的指针。
+返回值：
+无
+*/
+#include <stdio.h>  
+#include <unistd.h>  
+#include <pthread.h>  
+void *thread(void *arg)  
+{  
+    static int num = 456; //静态变量  
+    int i = 0;  
+    while(1)  
+    {  
+        printf("I am runing\n");  
+        sleep(1);  
+        i++;  
+        if(i==3)  
+        {  
+            pthread_exit( (void *)&num ); 
+        }  
+    }  
+    return NULL;  
+}  
+int main(int argc, char *argv[])  
+{  
+    int ret  = 0;  
+    pthread_t tid;  
+    void *value  = NULL;  
+      
+    ret = pthread_create(&tid, NULL, thread, NULL);    
+    if(ret!=0)
+    {  
+        perror("pthread_create");  
+    }  
+      
+    pthread_join(tid, &value);  
+      
+    printf("value = %d\n", *(int *)value );  
+      
+    return 0;  
+}  
+ 
